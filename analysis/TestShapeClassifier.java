9b3ace47860fb968c09bc7f52fb4b890c1c16fb9
iamadawra <aayushgopaldawra@gmail.com>
2016-02-20 18:21:15 -0500

    Added Setup and Teardown

---
+++ analysis/TestShapeClassifier.java
@@ -0,0 +1,34 @@
+package analysis;
+import junit.framework.TestCase;
+import org.junit.*;
+import java.util.*;
+import static org.junit.Assert.*;
+
+public class TestShapeClassifier {
+
+	private final String YES = "Yes";
+	private final String NO = "No";
+
+	private ShapeClassifier shapeClassifier; 
+	private String result;
+
+	@Before
+    public void setUp() {
+    	// Spawn a new ShapeClassifier instance
+        shapeClassifier = new ShapeClassifier();
+    }
+
+    @After
+    public void tearDown() {
+    	// Reset ShapeClassifier
+        shapeClassifier = null;
+    }
+
+
+    @Test
+    public void testBaselinePath() {
+		result = shapeClassifier.evaluateGuess("Rectangle,Large,Yes,100,20,100,20");
+        assertEquals(YES, result);
+    }
+
+}
